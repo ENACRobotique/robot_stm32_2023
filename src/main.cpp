81646c6096484e42b71fdf1fb4d433b31f2baeab
ENAC-Robotique <robotique.enac@gmail.com>
2022-11-24 15:14:51 +0100

    feat: serial logging without input

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <Arduino.h>
+#include "utilities/logging.h"
 
 #define LED 13
 
+int i = 0;
+
 //Arduino LED1 blink
 void setup() {
-  pinMode(LED, OUTPUT);
+    pinMode(LED, OUTPUT);
+
+    Logging::init(115200);
+    Logging::info("Démarrage du robot bas niveau v0.1.0");
+
+    // TODO: Logging::get()
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
+    i++;
+
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+
+    Logging::debug("Fin de la boucle %d", i);
 }
---
+++ src/utilities/logging.h
@@ -0,0 +1,27 @@
+#ifndef LOGGING_HEADER
+#define LOGGING_HEADER
+
+#include <stdarg.h>
+
+#define LOGGING_BUFFER 200
+
+
+
+class Logging {
+public:
+    static void init(unsigned long speed);
+
+    static void trace(const char* message, ...);
+    static void debug(const char* message, ...);
+    static void info(const char* message, ...);
+    static void warning(const char* message, ...);
+    static void error(const char* message, ...);
+    static void critical(const char* message, ...);
+
+private:
+    static void _log(const char *severity, const char *message, va_list args);
+
+    static char _buffer[LOGGING_BUFFER];
+};
+
+#endif // LOGGING_HEADER
