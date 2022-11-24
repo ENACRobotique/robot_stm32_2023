fe0d12fadf416073c33252fb2c7fa16248faf671
KirrimK <KirrimK@users.noreply.github.com>
2022-11-24 07:15:05 -0500

    sample led blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
+//Arduino LED1 blink
 void setup() {
-  
+  pinMode(LED1, OUTPUT);
 }
 
 void loop() {
-  
+  digitalWrite(LED1, HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  delay(1000);
 }
