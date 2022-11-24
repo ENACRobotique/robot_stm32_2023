82bdc05dcb7aeed6951834ab5a0caa02388ad90c
ENAC-Robotique <robotique.enac@gmail.com>
2022-11-24 13:33:25 +0100

    blink works

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 
+#define LED 13
+
 //Arduino LED1 blink
 void setup() {
-  pinMode(LED1, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH);
+  digitalWrite(LED, HIGH);
   delay(1000);
-  digitalWrite(LED1, LOW);
+  digitalWrite(LED, LOW);
   delay(1000);
 }
