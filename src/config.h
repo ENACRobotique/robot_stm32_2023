12a148d74155ceb89c187b2630809208cfd4ea2d
Thanatos973 <103666628+Thanatos973@users.noreply.github.com>
2022-11-24 17:54:51 +0100

    début code encoder

--- src/config.h
+++ src/config.h
@@ -3,11 +3,18 @@
 
 #include <Arduino.h>
 
-#define MOTOR_A_PWM
-#define MOTOR_A_DIR 
-#define MOTOR_B_PWM 
-#define MOTOR_B_DIR 
-#define MOTOR_C_PWM 
-#define MOTOR_C_DIR 
+#define MOTOR_1_PWM
+#define MOTOR_1_DIR 
+#define MOTOR_2_PWM 
+#define MOTOR_2_DIR 
+#define MOTOR_3_PWM 
+#define MOTOR_3_DIR 
+
+#define ENCODER_1_A PA_0
+#define ENCODER_1_B PA_1
+#define ENCODER_2_A PA_8
+#define ENCODER_2_B PB_10
+#define ENCODER_3_A
+#define ENCODER_3_B
 
 #endif // CONFIG_HEADER
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include "utilities/logging.h"
 #include "utilities/timer.h"
+#include "encoder.h"
+#include "config.h"
 
 #define LED 13
 
 int i = 0;
 Timer timer(1000);
+Encoder encoder1(ENCODER_1_A, ENCODER_1_B);
+Encoder encoder2(ENCODER_2_A, ENCODER_2_B);
 
 //Arduino LED1 blink
 void setup() {
@@ -15,15 +19,23 @@ void setup() {
     Logging::info("Démarrage du robot bas niveau v0.1.0");
 
     // TODO: Logging::get()
+
+    encoder1.init();
+    encoder2.init();
+
+    Logging::info("Initialisation Encoder");
+
 }
 
 void loop() {
-    if (timer.finished()) {
-        i++;
+    // if (timer.finished()) {
+    //     i++;
         
-        digitalWrite(LED, !digitalRead(LED));
-        timer.reset();
+    //     digitalWrite(LED, !digitalRead(LED));
+    //     timer.reset();
 
-        Logging::debug("Fin de la boucle %d", i);
-    }
+    //     Logging::debug("Fin de la boucle %d", i);
+    // }
+    Logging::trace("Encoder 1 : %d", encoder1.get_value());
+    Logging::trace("Encoder 2 : %d", encoder2.get_value());
 }
