cf39bdfc678e2e729f964125ab49032bec7e2455
ENAC-Robotique <robotique.enac@gmail.com>
2022-11-24 16:07:11 +0100

    feat: pin config file

---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_HEADER
+#define CONFIG_HEADER
+
+#include <Arduino.h>
+
+#define MOTOR_A_PWM
+#define MOTOR_A_DIR 
+#define MOTOR_B_PWM 
+#define MOTOR_B_DIR 
+#define MOTOR_C_PWM 
+#define MOTOR_C_DIR 
+
+#endif // CONFIG_HEADER
