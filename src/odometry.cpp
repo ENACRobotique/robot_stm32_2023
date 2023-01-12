d8ecfb7cc32ef021f9083fa1fd8cc0097843a94b
ENAC-Robotique <robotique.enac@gmail.com>
2023-01-12 18:57:19 +0100

    .

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -2,13 +2,5 @@
 
 void Odometry::update(){
 
-    // récup val chaque encoder
-    //déduire la vitesse en divisant la valeur par la différence de temp millis (arduino .h) - lastMillis
-    // mettre a jour lastmillis 
-    //mettre set val v1,v2,v3
-    //mutiplier par la matrix motors_to_axis
-    //set ensuite vx_robot, vy_robot, vz_robot
-
-     
 
 }
