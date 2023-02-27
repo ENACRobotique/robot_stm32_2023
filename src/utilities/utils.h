6e4de087a40197f19e0e64450236eac2968519c2
Wall-e_@_ClubRobotique <robotique.enac@gmail.com>
2023-02-27 19:40:02 +0100

    passage de distance en inline

--- src/utilities/utils.h
+++ src/utilities/utils.h
@@ -6,6 +6,6 @@ T clamp(T inf, T sup, T x) {
     return min(sup, max(inf, x));
 }
 
-float distance(float x1, float y1, float x2, float y2) {
+inline float distance(float x1, float y1, float x2, float y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
