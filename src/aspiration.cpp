ed431c56ba2a71d6a0e5c6d7841c9bec80a62036
Rayan JAGPAL <jagpalrayan973@gmail.com>
2023-04-16 22:02:40 +0200

    Add aspiration file

---
+++ src/aspiration.cpp
@@ -0,0 +1,6 @@
+#include "aspiration.h"
+
+void Aspiration::init(){
+    
+}
+
---
+++ src/aspiration.h
@@ -0,0 +1,20 @@
+#ifndef ASPIRATION_H
+#define ASPIRATION_H
+
+#include "Servo.h"
+
+
+class Aspiration{
+    public:
+        Aspiration(){
+            
+        }
+
+    void init();
+
+};
+
+
+//write millisecond
+
+#endif
