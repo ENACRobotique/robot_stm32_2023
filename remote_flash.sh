a78c45feb509d94acebe29d20c48b1ee9504fc92
Vitry Hugo <hugo.louis.vitry@gmail.com>
2023-04-25 15:36:52 +0200

    modif addr

--- remote_flash.sh
+++ remote_flash.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# hostname ou adresse ip de Cooking Mama
+COOKING_MAMA=cooking-mama
+
 echo "firmware path: $1"
 
-ssh pi@cooking-mama 'mkdir -p ~/firmware_bas_niveau'
-ssh pi@cooking-mama 'rm firmware_bas_niveau/*'
-scp $1 pi@cooking-mama:~/firmware_bas_niveau/
-ssh pi@cooking-mama 'st-flash --reset write ~/firmware_bas_niveau/firmware.bin 0x8000000'
+ssh pi@$COOKING_MAMA 'mkdir -p ~/firmware_bas_niveau'
+ssh pi@$COOKING_MAMA 'rm firmware_bas_niveau/*'
+scp $1 pi@$COOKING_MAMA:~/firmware_bas_niveau/
+ssh pi@$COOKING_MAMA 'st-flash --reset write ~/firmware_bas_niveau/firmware.bin 0x8000000'
 
